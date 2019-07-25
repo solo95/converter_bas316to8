445468f06006722042bc462f3de95bec3cacafd2
solo95 <swsimutowe@gmail.com>
2019-07-25 14:18:03 +0300

    Add files via upload

---
+++ WindowsFormsApplication8/WindowsFormsApplication8/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace WindowsFormsApplication8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.button1.Location = new System.Drawing.Point(529, 193);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Çevir";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Lime;
+            this.textBox1.Location = new System.Drawing.Point(48, 193);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(190, 29);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.Lime;
+            this.textBox2.Location = new System.Drawing.Point(294, 193);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(173, 29);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(106, 153);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Onaltılı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label2.Location = new System.Drawing.Point(340, 153);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Sekizli";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label3.Location = new System.Drawing.Point(44, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(374, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Onaltılından  Sekizliye çeviren program";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label4.Location = new System.Drawing.Point(106, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(291, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Adi Soyadi : Solomon William Simutowe";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label5.Location = new System.Drawing.Point(107, 50);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(213, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Öğrenci No : 121620131096";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowText;
+            this.ClientSize = new System.Drawing.Size(640, 290);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WindowsFormsApplication8/WindowsFormsApplication8/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string digit, Binary = "", Octa = "";
+                int n, number;
+                digit = textBox1.Text;
+                n = digit.Length;
+                for (int i = n - 1; i >= 0; i--)
+                {
+                    Binary = toBinary(digit[i]) + Binary;
+                }
+
+                while (Binary.Length % 3 != 0)
+                {
+                    Binary = "0" + Binary;
+                }
+
+                int L = Binary.Length;
+
+                for (int i = 0; i < L; i = i + 3)
+                {
+                    Octa += toOcta(Binary.Substring(i, 3));
+
+                }
+                number = Convert.ToInt32(Octa);
+                textBox2.Text = number.ToString();
+            }
+            catch(Exception){
+                MessageBox.Show("onaltılı bir sayı giriniz");
+            }
+        }
+
+        string toBinary(char x)
+        {
+
+            switch (x)
+            {
+                case '0':
+                    return "0000";
+                    break;
+                case '1':
+                    return "0001";
+                    break;
+                case '2':
+                    return "0010";
+                    break;
+                case '3':
+                    return "0011";
+                    break;
+                case '4':
+                    return "0100";
+                    break;
+                case '5':
+                    return "0101";
+                    break;
+                case '6':
+                    return "0110";
+                    break;
+                case '7':
+                    return "0111";
+                    break;
+                case '8':
+                    return "1000";
+                    break;
+                case '9':
+                    return "1001";
+                    break;
+                case 'A':
+                    return "1010";
+                    break;
+                case 'B':
+                    return "1011";
+                    break;
+                case 'C':
+                    return "1100";
+                    break;
+                case 'D':
+                    return "1101";
+                    break;
+                case 'E':
+                    return "1110";
+                    break;
+                case 'F':
+                    return "1111";
+                    break;
+                case 'a':
+                    return "1010";
+                    break;
+                case 'b':
+                    return "1011";
+                    break;
+                case 'c':
+                    return "1100";
+                    break;
+                case 'd':
+                    return "1101";
+                    break;
+                case 'e':
+                    return "1110";
+                    break;
+                case 'f':
+                    return "1111";
+                    break;
+                default:
+               
+                return "";
+                    break;
+
+            }
+
+        }
+        string toOcta(string bin)
+        {
+            switch (bin)
+            {
+                case "000":
+                    return "0";
+                    break;
+                case "001":
+                    return "1";
+                    break;
+                case "010":
+                    return "2";
+                    break;
+                case "011":
+                    return "3";
+                    break;
+                case "100":
+                    return "4";
+                    break;
+                case "101":
+                    return "5";
+                    break;
+                case "110":
+                    return "6";
+                    break;
+                case "111":
+                    return "7";
+                    break;
+                default:
+                    
+                  return "";
+                    break;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
